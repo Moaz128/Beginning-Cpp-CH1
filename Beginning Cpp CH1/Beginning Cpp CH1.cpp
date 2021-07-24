c2a1ac48f5d16c73ae1435da467d5dbfaf2ae097
Moaz128 <red.moaz@gmail.com>
2021-07-24 12:24:46 +0300

    Added a using directive for std

--- Beginning Cpp CH1/Beginning Cpp CH1.cpp
+++ Beginning Cpp CH1/Beginning Cpp CH1.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+//Included a using directive
+using namespace std;
 
 int main()
 {
-    std::cout << "Game Over!" <<std::endl;
+    cout << "Game Over!" << endl;
     return 0;
 }
